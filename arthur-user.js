0f480f795afa4cf432dae0f8b7e0c7e14fd195a0
contact <contact@artpologabriel.com>
2020-10-30 22:42:44 -0700

    up

--- arthur-user.js
+++ arthur-user.js
@@ -1,6 +1,6 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/kyle-user')
+const User = require('./models/user-kyle')
 
 const app = express()
 const port = process.env.PORT || 3000
