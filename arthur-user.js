5d7434f22a5c3c24e0ce80f6c43aa3ffe145157a
contact <contact@artpologabriel.com>
2020-10-30 22:12:59 -0700

    up

--- arthur-user.js
+++ arthur-user.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users', (req, res) => {
+app.post('/kyle', (req, res) => {
     const user = new User(req.body)
 
     user.save().then(() => {
