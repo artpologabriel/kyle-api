4de39aeb2c55905fc4273f6194a534dd75d83a27
contact <contact@artpologabriel.com>
2020-11-02 02:02:23 -0800

    up

--- routers/1task.js
+++ routers/1task.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const Task = require('../models/task')
+const Task = require('../models/1task')
 const router = new express.Router()
 
 router.post('/tasks', async (req, res) => {
