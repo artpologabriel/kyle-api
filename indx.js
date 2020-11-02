cbe94a4c3759cebbe2142f10522c15756f4d7989
contact <contact@artpologabriel.com>
2020-11-01 22:16:01 -0800

    up

--- indx.js
+++ indx.js
@@ -1,28 +1,30 @@
 const express = require('express')
-
 require('./db/mongoose')
-const Xlist = require('./models/xuser')
-const xlistRouter = require('./routers/xlist')
-
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
 
 const app = express()
 const port = process.env.PORT || 3000
+
 app.use(express.json())
-app.use(xlistRouter)
-
-
-app.get('/viewexs', (req, res) => {
-    Xlist.find({}).then( (users) => {
-       res.send(users)
-    }).catch( (e) => {
-       res.send(e)
-    })   
-})
-
-
-
-
+app.use(userRouter)
+app.use(taskRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+const bcrypt = require('bcryptjs')
+
+const myFunction = async () => {
+    const password = 'Red12345!'
+    const hashedPassword = await bcrypt.hash(password, 8)
+
+    console.log(password)
+    console.log(hashedPassword)
+
+    const isMatch = await bcrypt.compare('red12345!', hashedPassword)
+    console.log(isMatch)
+}
+
+myFunction()
