7c40c573ed11d7f378e203b2720990784d600472
contact <contact@artpologabriel.com>
2020-11-01 22:40:40 -0800

    up

--- indx.js
+++ indx.js
@@ -1,6 +1,6 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/user')
+const User = require('./models/xuser')
 
 const app = express()
 const port = process.env.PORT || 3000
