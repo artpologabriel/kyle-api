faf3c9f4bd5ca31d4e099712df909b186c938abe
contact <contact@artpologabriel.com>
2020-11-02 00:02:03 -0800

    up

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/taskmanagerapi', {
+mongoose.connect('mongodb://127.0.0.1:27017/taskuser', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
