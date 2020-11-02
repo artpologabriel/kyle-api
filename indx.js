fca5f4a085f93270108ec65a70eb8d4e398107ea
contact <contact@artpologabriel.com>
2020-11-01 18:02:59 -0800

    up

--- indx.js
+++ indx.js
@@ -13,15 +13,7 @@ app.use(express.json())
 app.use(xlistRouter)
 
 
-app.post('/userex', (req, res) => {
-    const xlist = new Xlist(req.body)
-    
-    xlist.save().then(() => {
-        res.send(xlist)
-    }).catch((e) => {
-        res.status(400).send(e)
-    })
-})
+
 
 app.get('/viewexs', (req, res) => {
     Xlist.find({}).then( (users) => {
@@ -31,60 +23,7 @@ app.get('/viewexs', (req, res) => {
     })   
 })
 
-app.get('/userexs/:id', async (req, res) => {
-    const _id = req.params.id
 
-    try {
-        const xlist = await Xlist.findById(_id)
-
-            res.send(xlist)
-    } catch (e) {
-        res.send(e)
-    }
-
-})
-
-app.delete('/userexs/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try {
-        const xlist = await Xlist.findByIdAndDelete(_id)
-
-                if (!xlist){
-                    return res.status(404).send()
-                }
-
-
-            res.send(xlist)
-    } catch (e) {
-        res.send(e)
-    }
-
-})
-
-app.post('/userupdateex', async (req, res) => {
-    // const updates = Object.keys(req.body)
-     const _id  = req.body._id
-     const date = req.body.date
-     const reason = req.body.reason
-     const name = req.body.name
-
-    Xlist.findOneAndUpdate({_id},  
-        {reason, date , name}, null, function (err, docs) { 
-        if (err){ 
-            console.log(err) 
-            res.status(400).send()
-        } 
-        else{ 
-            console.log("Original Doc : ",docs)
-            res.send(docs)
-        } 
-    }); 
-
-
-  
-
-})
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
--- routers/xlist.js
+++ routers/xlist.js
@@ -1,10 +1,69 @@
 const express = require ('express')
+const Xlist = require ('../models/xuser')
 const router = new express.Router()
 
-router.get('/test',(req,res)=>{
-    res.send('this is router')
+router.post('/userex', (req, res) => {
+    const xlist = new Xlist(req.body)
+    
+    xlist.save().then(() => {
+        res.send(xlist)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
 })
 
+
+router.get('/userexs/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const xlist = await Xlist.findById(_id)
+
+            res.send(xlist)
+    } catch (e) {
+        res.send(e)
+    }
+
+})
+
+router.delete('/userexs/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const xlist = await Xlist.findByIdAndDelete(_id)
+
+                if (!xlist){
+                    return res.status(404).send()
+                }
+
+
+            res.send(xlist)
+    } catch (e) {
+        res.send(e)
+    }
+
+})
+
+router.post('/userupdateex', async (req, res) => {
+    // const updates = Object.keys(req.body)
+     const _id  = req.body._id
+     const date = req.body.date
+     const reason = req.body.reason
+     const name = req.body.name
+
+    Xlist.findOneAndUpdate({_id},  
+        {reason, date , name}, null, function (err, docs) { 
+        if (err){ 
+            console.log(err) 
+            res.status(400).send()
+        } 
+        else{ 
+            console.log("Original Doc : ",docs)
+            res.send(docs)
+        } 
+    }); 
+
+})
 
 
 
