38ea7aaf36178ebf4e960dc8b429c5bb59c58ae3
contact <contact@artpologabriel.com>
2020-11-01 22:34:30 -0800

    up

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/userexse', {
+mongoose.connect('mongodb://127.0.0.1:27017/api', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
 
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
