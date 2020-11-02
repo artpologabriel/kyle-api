00741f3d64208c075e4b4ec244c8ff38557dda51
contact <contact@artpologabriel.com>
2020-11-01 21:48:12 -0800

    up

--- routers/xlist.js
+++ routers/xlist.js
@@ -50,51 +50,53 @@ router.delete('/userexs/:id', async (req, res) => {
 
 })
 
-// router.post('/userupdateex', async (req, res) => {
-//     // const updates = Object.keys(req.body)
-//      const _id  = req.body._id
-//      const email = req.body.email
-//      const password = req.body.password
-//      const name = req.body.name
+router.post('/userupdateex', async (req, res) => {
+    // const updates = Object.keys(req.body)
+     const _id  = req.body._id
+     const email = req.body.email
+     const password = req.body.password
+     const name = req.body.name
      
 
-//     Xlist.findOneAndUpdate({_id},  
-//         {password, email , name}, null, function (err, docs) { 
-//         if (err){ 
-//             console.log(err) 
-//             res.status(400).send()
-//         } 
-//         else{ 
-//             console.log("Original Doc : ",docs)
-//             res.send(docs)
-//         } 
-//     }); 
+    Xlist.findOneAndUpdate({_id},  
+        {password, email , name}, null, function (err, docs) { 
+        if (err){ 
+            console.log(err) 
+            res.status(400).send()
+        } 
+        else{ 
+            console.log("Original Doc : ",docs)
+            res.send(docs)
+        } 
+    }); 
 
-// })
-router.post('/userupdateex', async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'email', 'password', 'age']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates!' })
-    }
-
-    try {
-        const xlist = await Xlist.findById(req.params.id)
-
-        updates.forEach((update) => xlist[update] = req.body[update])
-        await xlist.save()
-
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(users)
-    } catch (e) {
-        res.send(e)
-    }
 })
+
+
+// router.post('/userupdateex', async (req, res) => {
+//     const updates = Object.keys(req.body)
+//     const allowedUpdates = ['name', 'email', 'password', 'age']
+//     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+//     if (!isValidOperation) {
+//         return res.status(400).send({ error: 'Invalid updates!' })
+//     }
+
+//     try {
+//         const xlist = await Xlist.findById(req.params.id)
+
+//         updates.forEach((update) => xlist[update] = req.body[update])
+//         await xlist.save()
+
+//         if (!user) {
+//             return res.status(404).send()
+//         }
+
+//         res.send(users)
+//     } catch (e) {
+//         res.send(e)
+//     }
+// })
 
 
 
