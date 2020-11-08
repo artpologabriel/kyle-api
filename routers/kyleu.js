5a460c25f2fb176b6c25478364ff24cc346d5e78
contact <contact@artpologabriel.com>
2020-11-07 16:09:47 -0800

    up

--- routers/kyleu.js
+++ routers/kyleu.js
@@ -26,7 +26,9 @@ router.post('/users', async (req, res) => {
 router.post('/users/login', async (req, res)=>{
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
-        const token = await user.generateAuthToken()
+         const token = "kuytkuygtjhjhgjy" // await user.generateAuthToken()
+        console.log(user)
+        console.log(token)
         res.send({user,token})
     } catch (e){
         res.send(e)
