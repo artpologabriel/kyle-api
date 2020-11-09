a5611c237ee500220f8dd8337a1ff2a5ba0d7ae0
contact <contact@artpologabriel.com>
2020-11-08 17:08:26 -0800

    up

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/havey', {
+mongoose.connect('mongodb://127.0.0.1:27017/haveyhavey', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
--- routers/kyleu.js
+++ routers/kyleu.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const User = require('../models/kyleu')
 const auth = require ('../middleware/auth')
-const { remove } = require('../models/kyleu')
 const router = new express.Router()
 
 
