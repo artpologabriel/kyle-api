8f97432cb6d71892b2473af33a86c099fc9b2de1
contact <contact@artpologabriel.com>
2020-11-08 19:30:35 -0800

    up

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,21 @@
-const jwt = require ('jsonwebtoken')
-const User = require ('../models/kyleu')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
 
-
-
-const auth = async (req , res , next) =>{
+const auth = async (req, res, next) => {
     try {
-        const token = req.header('authorization').replace('Bearer', '')
-        const decoded = jwt.verify(token , 'thisismynewcourse')
-        const user = await User.findOne({_id: decoded_id, 'tokens.token': token})
+        const token = req.header('Authorization').replace('Bearer ', '')
+        const decoded = jwt.verify(token, 'thisismynewcourse')
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
 
-        if(!user){
+        if (!user) {
             throw new Error()
         }
+
         req.user = user
-    }catch (e) {
-        res.status(401).send({error : 'PLEASE AUTHENTICATE'})
-    }        
-    
+        next()
+    } catch (e) {
+        res.status(401).send({ error: 'Please authenticate.' })
+    }
 }
+
 module.exports = auth
--- routers/kyleu.js
+++ routers/kyleu.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const User = require('../models/kyleu')
+const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
 
@@ -25,7 +25,7 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
-router.get('/users/me',  auth,async (req, res) => {
+router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
 
