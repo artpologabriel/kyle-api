d0a05ca70de48d82198cee764c6f574f562cebd3
contact <contact@artpologabriel.com>
2020-11-08 17:04:19 -0800

    up

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/chingch', {
+mongoose.connect('mongodb://127.0.0.1:27017/huweh', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
