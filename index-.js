bf336d3902e9545ac217ec93f0521eb642bb972a
contact <contact@artpologabriel.com>
2020-11-08 17:34:10 -0800

    up

--- index-.js
+++ index-.js
@@ -30,3 +30,15 @@ app.listen(port, () => {
 // }
 
 // myFunction()
+
+// const jwt = require('jsonwebtoken')
+
+// const myFunction = async () => {
+//     const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '7 days' })
+//     console.log(token)
+
+//     const data = jwt.verify(token, 'thisismynewcourse')
+//     console.log(data)
+// }
+
+// myFunction()
