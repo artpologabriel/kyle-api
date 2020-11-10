cd96ccc1be616c8c0f74791d3e66a2b3bda167f3
contact <contact@artpologabriel.com>
2020-11-09 16:49:50 -0800

    up

--- routers/kyleu.js
+++ routers/kyleu.js
@@ -1,6 +1,5 @@
 const express = require('express')
-const User = require('../models/kyleu')
-const auth = require('../middleware/auth')
+const User = require('../models/user')
 const router = new express.Router()
 
 router.post('/users', async (req, res) => {
@@ -25,14 +24,13 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
-router.get('/users/me',auth, async (req, res) => {
-    res.send(req.user)
-    // try {
-    //     const users = await User.find({})
-    //     res.send(users)
-    // } catch (e) {
-    //     res.status(500).send()
-    // }
+router.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.send(users)
+    } catch (e) {
+        res.status(500).send()
+    }
 })
 
 router.get('/users/:id', async (req, res) => {
