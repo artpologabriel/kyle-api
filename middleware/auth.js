4e9669d1e8ba8936dc5b58cb901a43d04a1de89d
contact <contact@artpologabriel.com>
2020-11-09 18:04:59 -0800

    up

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,19 @@
 const jwt = require ('jsonwebtoken')
+const { findOne } = require('../models/kyleu')
 const User = require('../models/kyleu')
 
 
 const auth = async (req , res , next) => {
     try{
-        const token = req.header('Authorization')
-        console.log(token)
+        const token = req.header('Authorization').replace('Bearer ','')
+        const decoded = jwt.verify(token ,'thisismynewcourse')
+        const user = await findOne({ _id : decoded._id, 'tokens.token': token })
+
+        if(!user){
+            throw new Error()
+        }
+        req.user = user
+        next ()
     }catch (e){
         res.status(401).send({error : 'please log in'})
     }
