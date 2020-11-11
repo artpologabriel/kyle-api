979354adba3fdf13f7d1dd173833bfd550fbf293
contact <contact@artpologabriel.com>
2020-11-10 18:29:56 -0800

    up

--- models/kyleu.js
+++ models/kyleu.js
@@ -49,13 +49,13 @@ const userSchema = new mongoose.Schema({
     }]
 })
 
-userSchema.methods.getPublicProfile = function (){
+userSchema.methods.toJson = function (){
     const user = this
     const userObject = user.toObject()
 
     delete userObject.password
     delete userObject.tokens
-    
+
     return userObject
 }
 
